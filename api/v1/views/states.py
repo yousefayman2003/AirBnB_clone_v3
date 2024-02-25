6360df4c2785e57ff3414c4f10cf915a785a5652
Yousef Ayman <yousefayman20003@gmail.com>
2024-02-24 18:23:31 -0800

    Fixed bug

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,7 @@
 import uuid
 from flask import jsonify, abort, request
 from api.v1.views import app_views
+from models.state import State
 from models import storage
 from datetime import datetime
 
@@ -10,29 +11,29 @@ from datetime import datetime
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
 def list_all_states():
     """Retrieve a list of all State objects."""
-    states_list = [obj.to_dict() for obj in storage.all("State").values()]
-    return jsonify(states_list)
+    states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_single_state(state_id):
     """Retrieve a single State object."""
-    all_states = storage.all("State").values()
-    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if state_obj == []:
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+    if state == []:
         abort(404)
-    return jsonify(state_obj[0])
+    return jsonify(state[0])
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_single_state(state_id):
     """Delete a single State object."""
-    all_states = storage.all("State").values()
-    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if state_obj == []:
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+    if state == []:
         abort(404)
-    state_obj.remove(state_obj[0])
-    for obj in all_states:
+    state.remove(state[0])
+    for obj in states:
         if obj.id == state_id:
             storage.delete(obj)
             storage.save()
@@ -57,15 +58,15 @@ def create_single_state():
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_single_state(state_id):
     """Update a single State object."""
-    all_states = storage.all("State").values()
-    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
-    if state_obj == []:
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+    if state == []:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
-    state_obj[0]['name'] = request.json['name']
-    for obj in all_states:
+    state[0]['name'] = request.json['name']
+    for obj in states:
         if obj.id == state_id:
             obj.name = request.json['name']
     storage.save()
-    return jsonify(state_obj[0]), 200
+    return jsonify(state[0]), 200
