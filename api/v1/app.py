ec853923b36eb02ee87a0363024f95c856474ee8
Yousef Ayman <yousefayman20003@gmail.com>
2024-02-24 17:01:46 -0800

    Revert changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,14 +12,9 @@ def teardown(exception):
     """Close the current storage session."""
     storage.close()
 
-@app.errorhandler(404)
-def not_found(error):
-    '''return render_template'''
-    return jsonify(error='Not found'), 404)
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.register_blueprint(app_views, url_prefix='/api/v1')
-    app.url_map.strict_slashes = False
     app.run(host=host, port=port, threaded=True)
