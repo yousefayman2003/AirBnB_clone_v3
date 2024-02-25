406827d763e931e2bc8399112095895dfebc40d9
Yousef Ayman <yousefayman20003@gmail.com>
2024-02-24 17:03:25 -0800

    Fix betty

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''adslfjsadklfjkl kljadsfkl dasjlk flkjasdf.'''
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 # This wildcard import is intentional
 from api.v1.views.index import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
